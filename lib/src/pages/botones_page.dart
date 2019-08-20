a10ec2f4170bffa5e15882dc6719746d872bb7a5
Luis Columna <luis.columna10@gmail.com>
2019-08-20 10:05:56 -0500

    sección de diseños terminado

--- lib/src/pages/botones_page.dart
+++ lib/src/pages/botones_page.dart
@@ -1,5 +1,8 @@
+
+
 import 'package:flutter/material.dart';
 import 'dart:math';
+import 'dart:ui';
 
 
 class BotonesPage extends StatelessWidget {
@@ -8,6 +11,7 @@ class BotonesPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+    
       body: Stack(
         children: <Widget>[
           _fondoApp(),
@@ -15,11 +19,13 @@ class BotonesPage extends StatelessWidget {
             child: Column(
               children: <Widget>[
                 _titulos(),
+                _botonesRedondeados()
               ],
             ),
           )
         ],
-      )
+      ),
+      bottomNavigationBar: _bottomNavigationBar(context)
     );
   }
 
@@ -86,4 +92,113 @@ class BotonesPage extends StatelessWidget {
         ),
     );
   }
+
+  Widget _bottomNavigationBar(BuildContext context){
+    
+    /* return Theme(
+      data:Theme.of(context).copyWith(
+
+        canvasColor: Color.fromRGBO(55, 57, 84, 1.0),
+        primaryColor: Colors.pinkAccent,
+        textTheme: Theme.of(context).textTheme.copyWith(caption: TextStyle(color: Color.fromRGBO(116, 117, 152, 1.0)))
+      ),
+      child:BottomNavigationBar(
+        items:[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_today, size: 30.0),
+            title: Container()
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_today, size: 30.0),
+            title: Container()
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_today, size: 30.0),
+            title: Container()
+          ),
+        ]
+        
+      ),
+
+      
+    ); */
+    return BottomNavigationBar(
+      fixedColor: Colors.pinkAccent,
+      backgroundColor: Color.fromRGBO(55, 57, 84, 1.0),
+      unselectedItemColor: Color.fromRGBO(116, 117, 152, 1.0),
+      items: <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          icon: Icon(Icons.calendar_today),
+          title: Container()
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.bubble_chart),
+          title: Container()
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.supervised_user_circle),
+          title: Container()
+        ),
+      ],
+    );
+
+  }
+
+  Widget _botonesRedondeados() {
+    
+    return Table(
+      children: [
+        TableRow(
+          children: [
+           _crearBoton(Colors.blue, Icons.border_all, 'General'),
+           _crearBoton(Colors.purpleAccent, Icons.directions_bus, 'Bus'),
+          ]
+        ),
+        TableRow(
+          children: [
+          _crearBoton(Colors.pinkAccent, Icons.shop, 'Buy'),
+           _crearBoton(Colors.orange, Icons.insert_drive_file, 'File'),
+          ]
+        ),
+        TableRow(
+          children: [
+          _crearBoton(Colors.blueAccent, Icons.movie_filter, 'Entertaiment'),
+          _crearBoton(Colors.green, Icons.cloud, 'Cloud'),
+          ]
+        )
+      ],
+    );
+  }
+
+  Widget _crearBoton(Color color , IconData icono, String texto){
+
+    return BackdropFilter(
+      filter: ImageFilter.blur(sigmaX:0.0, sigmaY:0.0),//el blur es muy pesado consume muchos recursos
+      child: Container(
+        height: 180.0,
+        margin: EdgeInsets.all(15.0),
+        decoration: BoxDecoration(
+          color: Color.fromRGBO(62, 66, 107, 0.7),
+          borderRadius: BorderRadius.circular(20.0)
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            SizedBox(height: 5.0),
+            CircleAvatar(
+              backgroundColor: color,
+              radius: 35.5,
+              child: Icon(icono, color: Colors.white,size: 30.0,),
+            ),
+            Text(texto, style:TextStyle(color:color)),
+            SizedBox(height: 5.0)
+            
+          ],
+        ),
+      ),
+    );
+
+  }
+
+
 }
